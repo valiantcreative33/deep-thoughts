8f314044ec5abd0b04c2f11da4a44701a80269a1
Ruben Matamoros <ruben@valiantcreative.net>
2021-03-29 00:03:40 -0400

    created mutations for thoughts, friends, and reactions

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+// import the middleware function
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -11,7 +13,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
